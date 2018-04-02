01b1e18e439d06381939e1c4ffdf9d256e500def
James Logue <logueja@gmail.com>
2018-04-02 11:11:42 -0600

    Add GET /api/v1/items tests

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -38,14 +38,13 @@ describe('Client Routes', () => {
   it('should return 404 for a page that does not exist', () => {
     return chai.request(server)
       .get('/nopagehere')
-      .then(response => {
+      .then( response => {
         response.should.have.status(404)
       })
       .catch( error => {
         throw error
       })
   })
-
 })
 
 describe('Api Routes', () => {
@@ -59,6 +58,27 @@ describe('Api Routes', () => {
           done()
         })
       })
+    })
+  })
+
+  describe('GET /api/v1/items', () => {
+    it('should return all mars items', () => {
+      return chai.request(server)
+        .get('api/v1/items')
+        .then( response => {
+          response.should.have.status(200)
+          response.should.be.json
+          response.body.should.be.a('array')
+          response.body.length.should.equal(4)
+
+          response.body[0].should.have.property('item_name')
+          response.body[0].item_name.should.equal('')
+          response.body[0].should.have.property('packed')
+          response.body[0].packed.should.equal(true)
+        })
+        .catch( error => {
+          throw error
+        })
     })
   })
 
