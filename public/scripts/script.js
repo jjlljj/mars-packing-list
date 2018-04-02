2dd57c3ba3a460ede69030dda8878e7b7827ea15
James Logue <logueja@gmail.com>
2018-04-02 10:19:58 -0600

    Initial style of form section, link to scripts for interaction

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,3 +1,13 @@
 const addItem = event => {
   event.preventDefault()
+
+  const itemInput = document.querySelector('.item-input')
+
+  const newItem = { item_name: itemInput.value, packed: false }
+  console.log(newItem)
+
+}
+
+const renderCard = item => {
+
 }
