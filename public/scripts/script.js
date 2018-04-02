087cd093f1dda242367ffd22aaf597b0c3dc3c02
James Logue <logueja@gmail.com>
2018-04-02 13:12:10 -0600

    Add card render and delete

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,13 +1,65 @@
-const addItem = event => {
+const addItem = async event => {
   event.preventDefault()
 
   const itemInput = document.querySelector('.item-input')
 
   const newItem = { item_name: itemInput.value, packed: false }
-  console.log(newItem)
+  const posted = await postItem(newItem)
+  console.log(posted)
+  renderItemCard(posted)
 
 }
 
-const renderCard = item => {
+const postItem = async item => {
+  const posted = await fetch('/api/v1/items', {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(item)
+  })
+  if ( posted.status === 201 ) {
+    return await posted.json()
+  } else {
+    throw new Error('could not add item')
+  }
+}
+
+
+const renderItemCard = ({ id, item_name, packed }) => {
+  const marsCardSection = document.querySelector('.mars-cards-section')
+  const itemCard = document.createElement('div')
+  itemCard.setAttribute('class', `item${id} item-card`) 
+
+  itemCard.innerHTML = `
+    <h2>${item_name}</h2>
+    <button
+      class="delete-btn"
+      name="${id}"
+      onclick="deleteItem(event)"
+    >Delete</button>
+    <input 
+      type="checkbox"
+      class="toggle-packed"
+      >Packed</input>
+  `
+  marsCardSection.appendChild(itemCard)
+}
+
+const deleteItem = event => {
+  event.preventDefault()
+  const id = event.target.name
+
+  deleteItemCard(id)
+  deleteItemFetch(id)
+}
+
+const deleteItemCard = id => {
+  const itemCard = document.querySelector(`.item${id}`)
+  const marsCardSection = document.querySelector('.mars-cards-section')
+
+  marsCardSection.removeChild(itemCard)
+}
+
+
+const deleteItemFetch = id => {
 
 }
