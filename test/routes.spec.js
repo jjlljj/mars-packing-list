3fabdf6e9eb15ab7d78b52436241cfb895ccdbd9
James Logue <logueja@gmail.com>
2018-04-02 09:32:11 -0600

    Setup test framework

---
+++ test/routes.spec.js
@@ -0,0 +1,20 @@
+const chai = require('chai')
+const should = chai.should()
+const chaiHttp = require('chai-http')
+const server = require('../server')
+
+chai.use(chaiHttp)
+
+const environment = process.env.NODE_ENV || 'test'
+const configuration = require('../knexfile')[environment]
+const db = require('knex')(configuration)
+
+describe('Client Routes', () => {
+
+
+})
+
+describe('Api Routes', () => {
+
+
+})
